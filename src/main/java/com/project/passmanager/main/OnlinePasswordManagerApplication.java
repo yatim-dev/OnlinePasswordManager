11d064c70179e49f1a3a6c9ad0ed380eb0150c9d
Alexandr <alex.andros.solovev@gmail.com>
2023-10-01 00:47:02 +0500

    first commit

---
+++ src/main/java/com/project/passmanager/main/OnlinePasswordManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.passmanager.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePasswordManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePasswordManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/passmanager/OnlinePasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.passmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
