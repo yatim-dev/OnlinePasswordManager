2984b7ac55b525daec75effa90003cff15c9d50c
yatim-dev <yatim2002@gmail.com>
2023-10-07 12:18:57 +0500

    password generator is singleton and add documentation

--- src/main/java/com/project/passmanager/main/algorithms/PasswordGenerator.java
+++ src/main/java/com/project/passmanager/main/algorithms/PasswordGenerator.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Класс `PasswordGenerator` предоставляет возможность генерировать пароли с разными параметрами.
+ * Пользователь может настроить включение/выключение использования нижнего регистра, верхнего регистра,
+ * цифр и специальных символов при генерации пароля.
+ */
 public class PasswordGenerator {
+    private static PasswordGenerator instance;
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String DIGITS = "0123456789";
@@ -15,28 +21,67 @@ public class PasswordGenerator {
     private boolean useDigits = false;
     private boolean useSpecialCharacters = false;
 
-    public PasswordGenerator useLower(){
+    /**
+     * Приватный конструктор, чтобы предотвратить создание экземпляров извне.
+     */
+    private PasswordGenerator() {}
+
+    public static PasswordGenerator getInstance() {
+        if (instance == null) {
+            instance = new PasswordGenerator();
+        }
+        return instance;
+    }
+
+    /**
+     * Включает использование символов нижнего регистра при генерации пароля.
+     *
+     * @return Объект `PasswordGenerator` с включенным использованием символов нижнего регистра.
+     */
+    public PasswordGenerator useLower() {
         this.useLower = true;
         return this;
     }
 
-    public PasswordGenerator useUpper(){
+    /**
+     * Включает использование символов верхнего регистра при генерации пароля.
+     *
+     * @return Объект `PasswordGenerator` с включенным использованием символов верхнего регистра.
+     */
+    public PasswordGenerator useUpper() {
         this.useUpper = true;
         return this;
     }
 
-    public PasswordGenerator useDigits(){
+    /**
+     * Включает использование цифр при генерации пароля.
+     *
+     * @return Объект `PasswordGenerator` с включенным использованием цифр.
+     */
+    public PasswordGenerator useDigits() {
         this.useDigits = true;
         return this;
     }
 
-    public PasswordGenerator useSpecialCharacters(){
+    /**
+     * Включает использование специальных символов при генерации пароля.
+     *
+     * @return Объект `PasswordGenerator` с включенным использованием специальных символов.
+     */
+    public PasswordGenerator useSpecialCharacters() {
         this.useSpecialCharacters = true;
         return this;
     }
 
-    public String build(int length){
-        if (length <= 0) {
+    /**
+     * Генерирует пароль заданной длины с учетом настроек.
+     *
+     * @param length Длина генерируемого пароля.
+     * @return Сгенерированный пароль.
+     */
+    public String build(int length) {
+        if ((length <= 0) || (!useLower && !useUpper && !useDigits && !useSpecialCharacters)) {
+            resetFlags();
             return "";
         }
 
@@ -65,12 +110,21 @@ public class PasswordGenerator {
             int position = random.nextInt(charCategory.length());
             password.append(charCategory.charAt(position));
         }
-        useLower = false;
-        useUpper = false;
-        useDigits = false;
-        useSpecialCharacters = false;
 
+        resetFlags();
         return new String(password);
+    }
+
+    /**
+     * Сбрасывает все установленные флаги использования символов (нижний регистр, верхний регистр,
+     * цифры, специальные символы) к исходному состоянию.
+     *
+     */
+    private void resetFlags() {
+        this.useLower = false;
+        this.useUpper = false;
+        this.useDigits = false;
+        this.useSpecialCharacters = false;
     }
 }
 
