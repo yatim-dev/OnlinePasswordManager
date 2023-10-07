43e24336ec461d280d1199ed91cfc46af099f627
yatim-dev <yatim2002@gmail.com>
2023-10-07 19:53:03 +0500

    add evaluatePasswordStrength method

--- src/main/java/com/project/passmanager/main/Entity/PasswordHandler.java
+++ src/main/java/com/project/passmanager/main/Entity/PasswordHandler.java
@@ -14,7 +14,8 @@ public class PasswordHandler implements IPasswordHandler {
     /**
      * Приватный конструктор, чтобы предотвратить создание экземпляров извне.
      */
-    private PasswordHandler() {}
+    private PasswordHandler() {
+    }
 
     public static PasswordHandler getInstance() {
         if (instance == null) {
@@ -53,5 +54,101 @@ public class PasswordHandler implements IPasswordHandler {
     @Override
     public Boolean isMatch(String candidate, String hashed) {
         return BCrypt.checkpw(candidate, hashed);
+    }
+
+    /**
+     * Оценивает сложность пароля на основе нескольких критериев и возвращает баллы.
+     *
+     * @param password Пароль для оценки.
+     * @return Баллы, оценивающие сложность пароля.
+     */
+    public int evaluatePasswordStrength(String password) {
+        int strength = 0;
+
+        // Критерий 1: Длина пароля
+        if (password.length() >= 8) {
+            strength += 2;
+        }
+
+        // Критерий 2: Дополнительный балл за более длинный пароль
+        if (password.length() >= 12) {
+            strength += 3;
+        }
+
+        // Критерий 3: Наличие цифр
+        if (containsDigits(password)) {
+            strength += 2;
+        }
+
+        // Критерий 4: Наличие букв в верхнем и нижнем регистре
+        if (containsLowerCaseAndUpperCase(password)) {
+            strength += 2;
+        }
+
+        // Критерий 5: Наличие специальных символов
+        if (containsSpecialCharacters(password)) {
+            strength += 3;
+        }
+
+        // Критерий 6: Отсутствие стандартных слов в пароле
+        if (!containsCommonWords(password)) {
+            strength += 4;
+        }
+
+        return strength;
+    }
+
+    /**
+     * Проверяет, содержит ли пароль цифры.
+     *
+     * @param password Пароль для проверки.
+     * @return true, если пароль содержит цифры, иначе - false.
+     */
+    private static boolean containsDigits(String password) {
+        return password.matches(".*\\d.*");
+    }
+
+    /**
+     * Проверяет, содержит ли пароль буквы в верхнем и нижнем регистре.
+     *
+     * @param password Пароль для проверки.
+     * @return true, если пароль содержит буквы в обоих регистрах, иначе - false.
+     */
+    private boolean containsLowerCaseAndUpperCase(String password) {
+        return password.matches(".*[a-z].*") && password.matches(".*[A-Z].*");
+    }
+
+    /**
+     * Проверяет, содержит ли пароль специальные символы.
+     *
+     * @param password Пароль для проверки.
+     * @return true, если пароль содержит специальные символы, иначе - false.
+     */
+    private boolean containsSpecialCharacters(String password) {
+        return password.matches(".*[!@#$%^&*()-_=+\\\\|\\[{\\]};:'\",<.>/?].*");
+    }
+
+
+    /**
+     * Проверяет, содержит ли пароль стандартные слова.
+     *
+     * @param password Пароль для проверки.
+     * @return true, если пароль содержит общие слова, иначе - false.
+     */
+    private boolean containsCommonWords(String password) {
+        // Список известных общих паролей
+        String[] commonPasswords = {"password", "admin", "123456", "qwerty", "letmein", "welcome", "123456789"};
+
+        // Приведем пароль к нижнему регистру для сравнения без учета регистра
+        password = password.toLowerCase();
+
+        // Проверяем, содержится ли пароль в списке известных общих паролей
+        for (String commonPassword : commonPasswords) {
+            if (password.contains(commonPassword)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
